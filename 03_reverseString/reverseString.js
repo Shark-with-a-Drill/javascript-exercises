0b402c9fd6bdea466b404ea7bb09e7897334a90f
Shark-with-a-Drill <108551190+Shark-with-a-Drill@users.noreply.github.com>
2023-01-07 16:06:10 -0800

    Do exercises 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if (!string) {
+        return string;
+    }
+    const invArr = string.split('');
+    invArr.reverse();
+    string = invArr.join('');
+    return string;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
